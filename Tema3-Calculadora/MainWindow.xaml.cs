d985352143043bbb4bd8b2723ab3557d9195fde0
IAN ESTEBAN TAUZY LAMAGNA <iantaulam@alu.edu.gva.es>
2022-10-17 11:47:25 +0200

    Terminado-CorregidoClase

--- Tema3-Calculadora/MainWindow.xaml.cs
+++ Tema3-Calculadora/MainWindow.xaml.cs
@@ -23,6 +23,24 @@ namespace Tema3_Calculadora
         public MainWindow()
         {
             InitializeComponent();
+            int num = 1;
+            for(int i = 1; i < 4; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Button b = new Button();
+                    Viewbox v = new Viewbox();
+                    TextBlock t = new TextBlock();
+                    t.Text = num.ToString();
+                    b.Tag = num.ToString();
+                    v.Child = t;
+                    b.Content = v;
+                    Grid.SetRow(b, i);
+                    Grid.SetColumn(b, j);
+                    ContenedorGrid.Children.Add(b);
+                    num++;
+                }
+            }
         }
 
         private void ClickButton(object sender, RoutedEventArgs e)
